ec85e6968a2f06034dba13bb4e76bda0d0e4fd4d
Gabriel S. Colombo <gscolombo404@gmail.com>
2026-03-22 11:57:47 -0300

    filtragem de URLs para download

--- data/chokepoint/loader.py
+++ data/chokepoint/loader.py
@@ -24,21 +24,25 @@ BLOCK_SIZE = 1024
 
 URL_LIST = List[List[str]]
 
+PREFIX = "P2"
+TO_DOWNLOAD = ["P2E_S2", "P2L_S2", "P2E_S1", "P2L_S1"]
+
+
 def download_file(dirpath: Path, url: str, tqdm_func, global_tqdm):
     req = requests.get(url, stream=True)
-    
+
     content_length = int(req.headers.get('content-length', 0))
-    
+
     if content_length == 0:
         return False
-        
-    with tqdm_func(total=content_length, 
-                   unit="B", 
-                   unit_scale=True, 
+
+    with tqdm_func(total=content_length,
+                   unit="B",
+                   unit_scale=True,
                    dynamic_ncols=True,
                    desc=dirpath.name,
                    leave=False) as pbar:
-        try: 
+        try:
             with open(dirpath, "wb") as file:
                 for chunk in req.iter_content(chunk_size=BLOCK_SIZE):
                     file.write(chunk)
@@ -47,11 +51,12 @@ def download_file(dirpath: Path, url: str, tqdm_func, global_tqdm):
             if dirpath.exists():
                 rmtree(dirpath)
             return (False, dirpath.name, e)
-    
+
     global_tqdm.update(1)
-    
+
     return True, None, None
-    
+
+
 def scrape_urls() -> URL_LIST:
     urls = []
 
@@ -63,11 +68,10 @@ def scrape_urls() -> URL_LIST:
         url = a.get("href")
         filename: str = url.split("/")[-1]
 
-        if "S" in filename:
-            urls.append([ORIGINAL_FILES_DIRNAME, url])
-            continue
+        if filename.startswith(PREFIX):
+            if filename.split(".", maxsplit=1)[0] in TO_DOWNLOAD:
+                urls.append([ORIGINAL_FILES_DIRNAME, url])
+            elif "S" not in filename:
+                urls.append([CROPPED_FACE_IMAGES_DIRNAME, url])
 
-        if not "groundtruth" in filename:
-            urls.append([CROPPED_FACE_IMAGES_DIRNAME, url])
-            
     return urls
