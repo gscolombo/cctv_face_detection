ba354c5fb7da1371e1ebdfb475189f6c50724ea3
Gabriel S. Colombo <gscolombo404@gmail.com>
2026-03-24 17:23:47 -0300

    Função para popular banco de dados MongoDB com embeddings e refatorações

--- data/chokepoint/processor.py
+++ data/chokepoint/processor.py
@@ -3,99 +3,112 @@ from tqdm import tqdm
 import numpy as np
 from pymongo import MongoClient
 from deepface.modules.representation import represent
+from deepface.modules.datastore import register
+from deepface.modules.exceptions import DuplicateEntryError
 
 import os
-import sys
 import tarfile as tar
 import io
 from pathlib import Path
-from pprint import pprint
 from shutil import rmtree
-import logging
-from typing import TypedDict
-
-logger = logging.getLogger(__name__)
+from PIL import Image
+from datetime import datetime
 
 BASE_PATH = Path(os.path.abspath(os.path.dirname(__file__)))
 OUTPUT_PATH = os.path.join(BASE_PATH, "videos")
 
-CODEC_FOURCC = cv.VideoWriter.fourcc(*'FFV1')
+CODEC_FOURCC = cv.VideoWriter.fourcc(*"FFV1")
 VIDEO_FORMAT = ".avi"
 
 FRAME_WIDTH = 800
 FRAME_HEIGHT = 600
 FRAME_RATE = 30.0
 
-MODEL_NAME = "Facenet"
+MODEL_NAME = "SFace"
 
-db_client = MongoClient(os.environ.get("MONGODB_URI"))
-
-
-class Person(TypedDict):
-    _id: int
-    embeddings: list[list[float]]
-
-    def __init__(_id: int, embeddings: list[list[float]]):
-        self._id = _id
-        self.embeddings = embeddings
+db_client = MongoClient(os.environ["MONGODB_URI"])
 
 
-def compose_video(name: str, frames: list, tqdm):
-    output = cv.VideoWriter(filename=os.path.join(OUTPUT_PATH, f"{name}{VIDEO_FORMAT}"),
-                            apiPreference=cv.CAP_FFMPEG,
-                            fourcc=CODEC_FOURCC,
-                            fps=FRAME_RATE,
-                            frameSize=(FRAME_WIDTH, FRAME_HEIGHT))
+def compose_video(name: str, frames: list):
+    try:
+        output = cv.VideoWriter(
+            filename=os.path.join(OUTPUT_PATH, name),
+            apiPreference=cv.CAP_FFMPEG,
+            fourcc=CODEC_FOURCC,
+            fps=FRAME_RATE,
+            frameSize=(FRAME_WIDTH, FRAME_HEIGHT),
+        )
 
-    with tqdm(frames,
-              desc=f"Composing video {name}",
-              unit="frame",
-              leave=False) as pbar:
-        for frame in frames:
-            output.write(cv.imread(frame))
-            pbar.update(1)
+        with tqdm(frames, desc=name, unit="frame", leave=False) as pbar:
+            for frame in frames:
+                output.write(cv.imread(frame))
+                pbar.update(1)
 
-    output.release()
+        output.release()
+    except Exception as e:
+        with open(BASE_PATH.joinpath("logs.txt", "a+")) as log:
+            log.write(f"ERROR@{datetime.now()}")
+            log.write(f"\nError during processing of file {name}")
+            log.write(e)
 
 
-def compose_chokepoint_video(path: Path, tqdm_func=None):
-    _tqdm = tqdm if tqdm_func is None else tqdm_func
+def compose_chokepoint_videos(path: Path):
+    # Prepare file system
     os.makedirs(OUTPUT_PATH, exist_ok=True)
 
     tmp_folder = BASE_PATH.joinpath("tmp")
     tmp_folder.mkdir(exist_ok=True)
 
+    # Open and extract tar file contents to temporary storage
     with tar.open(path, mode="r:xz") as xz:
-        xz.extractall(path=tmp_folder, filter='data')
+        print(f"Extracting {path.stem}")
+        xz.extractall(path=tmp_folder, filter="data")
 
-        with tqdm_func(total=len(xz.members), desc=f"File {xz.name.split("/")[-1]}") as inner_pbar:
-            for _tarfile in xz:
-                inner_tar_path = tmp_folder.joinpath(_tarfile.name)
+        for _tarfile in xz:
+            inner_tar_path = tmp_folder.joinpath(_tarfile.name)
+            video_name = f"{inner_tar_path.stem.split(".")[0]}{VIDEO_FORMAT}"
 
-                with tar.open(inner_tar_path, mode="r:xz") as inner_xz:
-                    inner_xz.extractall(path=tmp_folder, filter='data',
-                                        members=[m for m in inner_xz if m.name.endswith('.jpg')])
-                    os.remove(inner_tar_path)
+            if video_name in os.listdir(OUTPUT_PATH):
+                print(f"Video {video_name} already composed. Skipping.")
+                os.remove(inner_tar_path)
+                continue
 
-                    frame_folders = [Path(tmp_folder, folder) for folder in os.listdir(tmp_folder)
-                                    if folder.startswith(_tarfile.name.split(".")[0])]
+            # Open and extract inner tar files to temporary storage
+            with tar.open(inner_tar_path, mode="r:xz") as inner_xz:
 
-                    for folder in frame_folders:
-                        compose_video(folder.name,
-                                    [Path(folder, img)
-                                    for img in sorted(os.listdir(folder))],
-                                    tqdm_func)
+                print(f"Extracting {inner_tar_path.stem}")
+                inner_xz.extractall(
+                    path=tmp_folder,
+                    filter="data",
+                    members=[
+                        m
+                        for m in inner_xz
+                        if m.name.endswith(".jpg")  # Filter for .jpg files
+                    ],
+                )
+                os.remove(inner_tar_path)
 
-                        rmtree(folder)
-                        inner_pbar.update(1)
+                frame_folders = [
+                    tmp_folder.joinpath(folder)
+                    for folder in os.listdir(tmp_folder)
+                    if folder.startswith(_tarfile.name.split(".")[0])
+                ]
+
+                for folder in frame_folders:
+                    compose_video(
+                        video_name,
+                        [folder.joinpath(img) for img in sorted(os.listdir(folder))],
+                    )
+
+                    rmtree(folder)
 
 
-def populate_db(path: Path, global_tqdm):
+def populate_db(path: Path, _filter: list[str]):
     # Use database connection pool
     global db_client
 
     db = db_client.get_database(os.environ.get("DB_NAME"))
-    coll = db.get_collection(os.environ.get("DB_COLLECTION"))
+    persons_coll = db.get_collection("persons")
 
     # Prepare data for storage
     data: list[Person] = []
@@ -104,41 +117,70 @@ def populate_db(path: Path, global_tqdm):
     tmp_folder.mkdir(exist_ok=True)
 
     with tar.open(path, mode="r:xz") as xz:
-        print(f"Extracting {path.name}", end="\r")
-        xz.extractall(path=tmp_folder, filter='data')
+        print(f"Extracting {path.name}")
+        xz.extractall(
+            path=tmp_folder,
+            filter="data",
+            members=[
+                m
+                for m in xz
+                if m.name.endswith(".pgm") and any([f in m.name for f in _filter])
+            ],
+        )
 
-        video_folders: list[Path] = [Path(tmp_folder, folder) for folder in os.listdir(tmp_folder)
-                                     if folder.startswith(xz.name.split(".")[0])]
-
-        persons = []
+        folders: list[Path] = [
+            folder
+            for folder in tmp_folder.iterdir()
+            if os.path.basename(xz.name).split(".")[0] in os.path.basename(folder)
+        ]
 
         # Create embeddings for each id in each video
-        for folder in video_folders:
-            for _id in os.listdir(folder):
-                images = Path(folder, _id)
-                embeddings = [resp[0]["embedding"] for resp in
-                              represent(images, model_name=MODEL_NAME, enforce_detection=False, detector_backend="skip")]
+        last_id = 0
+        frame_interval = 5  #
 
-                persons.extend(Person(_id=_id, embeddings=embeddings))
+        for folder in tqdm(
+            folders, desc=f"{Path(xz.name).stem}", unit="folder", leave=False
+        ):
+            for _id in tqdm(
+                os.listdir(folder), desc=f"ID {folder.stem}", unit="id", leave=False
+            ):
+                f = -1
 
-                coll.update_one(
-                    {_id: _id},
-                    {"$push": {"embeddings": {"$each": embeddings}}},
-                    upsert=True
-                )
+                images = [
+                    np.array(Image.open(path, formats=["PPM"]).convert("RGB"))
+                    for path in Path(folder, _id).iterdir()
+                    if (f := f + 1) % frame_interval
+                ]
 
-    # Store data in database
+                embeddings = []
+                kwargs = {
+                    "model_name": MODEL_NAME,
+                    "enforce_detection": False,
+                    "detector_backend": "skip",
+                    "database_type": "mongo",
+                    "connection": db_client,
+                }
 
+                try:
+                    n = register(images, **kwargs)[
+                        "inserted"
+                    ]  # Store embeddings in DeepFace format
 
-def process_data(compose: bool, path: Path, tqdm_func, global_tqdm):
-    try:
-        if compose:
-            compose_chokepoint_video(path, tqdm_func)
-        else:
-            return True
+                    # Relate the inserted embeddings IDs with the person ID
+                    persons_coll.update_one(
+                        {"_id": _id},
+                        {
+                            "$push": {
+                                "embedding_id": {
+                                    "$each": list(range(last_id, last_id + n))
+                                }
+                            }
+                        },
+                        upsert=True,
+                    )
 
-        global_tqdm.update(1)
-        return True
-    except Exception as e:
-        logger.error(e)
-        return
+                    last_id += n
+                except DuplicateEntryError as e:
+                    continue
+
+            rmtree(folder)
