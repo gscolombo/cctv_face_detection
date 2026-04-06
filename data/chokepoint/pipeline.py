5893799ab77349f800747b231e7e40c9aeb715c2
Gabriel S. Colombo <gscolombo404@gmail.com>
2026-04-06 08:30:42 -0300

    Reorganização de código para pipeline de ingestão de dados

--- data/chokepoint/pipeline.py
+++ data/chokepoint/pipeline.py
@@ -1,19 +1,21 @@
 import os
 from shutil import rmtree
 
-from loader import extract_data, DOWNLOAD_PATH
+os.environ["CHOKEPOINT_DS_URL"] = "https://arma.sourceforge.net/chokepoint/"
+os.environ["CHOKEPOINT_FILES_TO_DOWNLOAD"] = "P1E_S1.tar.xz, P1E.tar.xz"
+os.environ["ORIGINAL_FILES_DIRNAME"] = "original_files"
+os.environ["CROPPED_FACE_IMAGES_DIRNAME"] = "cropped_face_images"
+os.environ["MONGODB_URI"] = "mongodb://root:root@localhost:27017/"
+os.environ["DB_NAME"] = "test"
+
 from processor import compose_chokepoint_videos, populate_db, BASE_PATH
+from loader import extract_data, DOWNLOAD_PATH
 
 from utils import clear_terminal
 
 
-if __name__ == "__main__":
-    os.environ["CHOKEPOINT_DS_URL"] = "https://arma.sourceforge.net/chokepoint/"
-    os.environ["CHOKEPOINT_FILES_TO_DOWNLOAD"] = "P1E_S1.tar.xz, P1E.tar.xz"
-    os.environ["ORIGINAL_FILES_DIRNAME"] = "original_files"
-    os.environ["CROPPED_FACE_IMAGES_DIRNAME"] = "cropped_face_images"
-    os.environ["MONGODB_URI"] = "mongodb://root:root@localhost:27017/"
 
+if __name__ == "__main__":
     clear_terminal()
     extract_data()
     clear_terminal()
